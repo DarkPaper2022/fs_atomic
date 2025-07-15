fc1a2f8f6d90224204fa19fd485fbf29a6474292
darkpaper <1529110395@qq.com>
2025-07-15 13:33:04 +0800

    Build

--- r.sh
+++ r.sh
@@ -1,2 +1,4 @@
+cmake -S . -B build && cmake --build ./build
+mkdir -p log
 strace -f -e trace=lseek,read,write -o log/strace.txt ./build/main > ./log/runtime.txt
 diff ./log/collected_data.txt ./log/standard_data.txt > ./log/diff.txt
